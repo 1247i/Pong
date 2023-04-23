f8d4c856177a524cab98305d62a41b6c8caaf952
1247i <130288038+1247i@users.noreply.github.com>
2023-04-23 16:32:02 +0500

    Add files via upload

--- 1.py
+++ 1.py
@@ -40,10 +40,13 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys [K_DOWN] and self.rect.y < win_h - 50:
             self.rect.y += self.speed
+
+
 racket1=Player('racket1.jpg',80,200,500,150,10)
 racket2=Player('racket1.jpg',920,200,500,150,10)
 ball=GameSprite('ball.png',300,200,50,150,10)
-
+speed_x=5
+speed_y=5
 finish=False
 
 run = True
@@ -58,12 +61,18 @@ while run:
         racket1.update_l()
         racket2.update_r()
         racket1.reset()
-        
+        ball.rect.x+=speed_x
+        ball.rect.y+=speed_y
+        if racket1.rect.colliderect(ball.rect) or racket2.rect.colliderect(ball.rect):
+            speed_x *= -1
+        if ball.rect.y > win_h -50 or ball.rect.y <0:
+            speed_y *= -1
+
         
         racket2.reset()
         
         ball.reset() 
         display.update() 
-    time.delay(60)      
+    time.delay(30)      
 
     
